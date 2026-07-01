52ee7d7ff00cf6669e3cacce0af85ed9afc5adcf
billkill <127614626+ChathuraRatnayake997@users.noreply.github.com>
2026-07-01 10:02:26 +0530

    rm

--- js/api.js
+++
@@ -1,8 +0,0 @@
-function fetchJSON(path) {
-    return fetch(path, {credentials: 'omit'}).then(function(resp) {
-        if (!resp.ok) throw new Error('Failed to load ' + path);
-        return resp.text();
-    }).then(function(text) {
-        return JSON.parse(text);
-    });
-}
